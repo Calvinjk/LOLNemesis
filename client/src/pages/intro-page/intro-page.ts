9c337ecee1bf2896ff84137a75c7156d1b1dd37a
Brody96 <brodymclarke@gmail.com>
2016-10-08 14:25:00 -0400

    now gets names for user stats

--- client/src/pages/intro-page/intro-page.ts
+++ client/src/pages/intro-page/intro-page.ts
@@ -11,16 +11,41 @@ export class IntroPage {
   public summoner = {
     name: ""
   }
-  public userArr: any[];
+  public userArr: any[] = [];
   public id: any;
+  public stats: any[] = [];
+  public namedStats: any[];
+  public champions: any[];
   constructor(public nav: NavController, public http: Http) {
   }
 
   pushNemesisPage(name) {
 
     this.http.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name + "?api_key=RGAPI-ce8f3488-0df4-4d9c-b42b-9c64b391b722").subscribe(data => {
-        // this.userArr = data;
-        console.log(data);
+      let str = data.text();
+      let splitVal = str.search(":");
+      var myArr = JSON.parse(str.slice(splitVal + 1, -1));
+      localStorage.setItem('id', (myArr.id).toString());
+    });
+
+    this.http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?dataById=true&api_key=RGAPI-ce8f3488-0df4-4d9c-b42b-9c64b391b722").map(res => res.json()).subscribe(data => {
+      let arr = data.data;
+      this.champions = Object.keys(arr).map(function(key) {return arr[key];});
+    });
+
+    this.http.get("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + localStorage.getItem('id') + "/ranked?season=SEASON2016&api_key=RGAPI-ce8f3488-0df4-4d9c-b42b-9c64b391b722").map(res => res.json()).subscribe(data => {
+      this.stats = data;
+      console.log(this.stats, this.champions);
+      for (var i=0; i < this.stats.champions.length; i++){
+        for (var j = 0; j < this.champions.length; j++){
+                  if(this.stats.champions[i].id == this.champions[j].id ){
+                    this.stats.champions[i].id = this.champions[j].name;
+                    console.log(this.stats.champions[i].id);
+                  }
+        }
+      }
+      console.log(this.stats);
+      this.nav.push(NemesisPage, { 'userInfo': this.stats })
     });
 
 
@@ -35,15 +60,15 @@ export class IntroPage {
     //     // console.log(myArr.id);
     //     localStorage.setItem('id', (myArr.id).toString());
     //     // console.log(localStorage.getItem('id'));
-        
+
     //   }
     // };
     // xmlhttp.open("GET", url, true);
     // xmlhttp.send();
 
-    
+
     // this.nav.push(NemesisPage);
-    
+
   };
 
 
--- client/src/pages/nemesis/nemesis.ts
+++ client/src/pages/nemesis/nemesis.ts
@@ -1,66 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
     templateUrl: 'nemesis.html'
 })
-export class NemesisPage implements OnInit{
+export class NemesisPage implements OnInit {
     public stats: any[];
     public champions: any[];
     constructor(public nav: NavController,
-        public params: NavParams) {
+        public params: NavParams,
+        public http: Http) {
 
     }
 
     ngOnInit() {
         
-        this.getStats();
-        console.log('statsssss', this.getStats());
-
-        this.champions = this.getChampions();
-
-        
-    }
-
-    getStats(): Observable<any> {
-        var xmlhttp1 = new XMLHttpRequest();
-        var url1 = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?dataById=true&api_key=RGAPI-ce8f3488-0df4-4d9c-b42b-9c64b391b722";
-
-        var myArr;
-
-        
-        return localStorage.getItem('id').then(() => {
-            xmlhttp1.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                return JSON.parse(this.responseText);
-                // console.log(this.stats);
-                // return myArr;
-            }
-        };
-        });
-        // xmlhttp1.open("GET", url1, true);
-        // xmlhttp1.send();
-    }
-
-    getChampions(): any{
-        console.log(localStorage.getItem('id'));
-        var xmlhttp = new XMLHttpRequest();
-        var url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + localStorage.getItem('id') + "/ranked?season=SEASON2016&api_key=RGAPI-ce8f3488-0df4-4d9c-b42b-9c64b391b722";
-
-        var myArr1;
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                myArr1 = JSON.parse(this.responseText);
-                this.champions = myArr1;
-            }
-        };
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
 
     }
+
     pushNemesisPage() {
-        console.log(this.stats);
+        
 
     };
 
