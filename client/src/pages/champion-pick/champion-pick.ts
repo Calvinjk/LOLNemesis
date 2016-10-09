4ce7e1e20c085a8b52cc144af00412d166ae02e7
Brody96 <brodymclarke@gmail.com>
2016-10-09 02:35:24 -0400

    can now see all champs

--- client/src/pages/champion-pick/champion-pick.ts
+++ client/src/pages/champion-pick/champion-pick.ts
@@ -3,27 +3,39 @@ import {NemesisPage} from '../nemesis/nemesis';
 import {Component, OnInit} from '@angular/core';
 import { Http } from '@angular/http';
 @Component({
-  templateUrl: 'champion-pick.html'
+    templateUrl: 'champion-pick.html'
 })
 export class ChampionPicksPage implements OnInit {
-    public champion: any;
     public champions: any;
-  constructor(public viewCtrl: ViewController,
-    public params: NavParams,
-    public http: Http) {
-  }
-  ngOnInit() {
-    // var arr = this.params.get('id');
-    this.http.get("/championImages.json").subscribe(data => {
-      let arr = JSON.parse(data.text());
-      console.log("here", arr);
-    });
-  }
-  dismiss() {
-      console.log(this.champions);
-    this.viewCtrl.dismiss();
-  }
-  insertChampion(champion) {
-    this.viewCtrl.dismiss(this.champion);
-  }
+    constructor(public viewCtrl: ViewController,
+        public params: NavParams,
+        public http: Http) {
+    }
+    ngOnInit() {
+        // var arr = this.params.get('id');
+        this.http.get("/championImages.json").subscribe(data => {
+            let arr = JSON.parse(data.text());
+            console.log(arr);
+            arr.sort();
+            arr.sort(function (a, b) {
+                if (a.championName > b.championName) {
+                    return 1;
+                }
+                if (a.championName < b.championName) {
+                    return -1;
+                }
+                // a must be equal to b
+                return 0;
+            });
+            console.log(arr);
+            this.champions = arr;
+        });
+    }
+    dismiss() {
+        console.log(this.champions);
+        this.viewCtrl.dismiss();
+    }
+    insertChampion(champion) {
+        this.viewCtrl.dismiss();
+    }
 }
